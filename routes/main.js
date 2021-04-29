33976a7243cad3a118348df71901ed8352caabea
BuysChips <43148665+BuysChips@users.noreply.github.com>
2021-04-28 21:25:36 -0400

    Now retrieves the location data from database

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const location = require('../data/location');
 
 router.get('/', async (req, res) => {
-    res.render('layouts/main');
+    let allLocations = await location.getAllLocations();
+    res.render('layouts/main', { locations: allLocations});
 });
 
 
