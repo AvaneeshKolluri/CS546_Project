292ec2ae59d1ed9df75b264cde2909d545795c87
Akhilesh Reddy <akhileshreddy96@gmail.com>
2021-05-03 18:44:37 -0400

    Validate LocationInfo Routes

--- public/js/ajax_locations.js
+++ public/js/ajax_locations.js
@@ -14,26 +14,42 @@
 	form_submit.submit(function(event) {
 		event.preventDefault();
 		error_div.empty();
-		let street_val = street.val();
-		let state_val = state.val();
-		let town_val = town.val();
-		let date_val = date.val();
 		
+		try{
+			
+			let street_val = street.val();
+			let state_val = state.val();
+			let town_val = town.val();
+			let date_val = date.val();
 
-		if (new Date(date_val) > new Date()){
-			error_div.append($('<p></p>').text("Date Has Not Yet Occured."));
-			error_div.addClass("error-userinfo");
-		}else{
+			//validate all fields
+
+			if (street_val.trim().length === 0){
+				throw "Must Enter A Valid Street Address."
+			}
+			if (town_val.trim().length === 0){
+				throw "Must Enter A Valid Town."
+			}
+			if (state_val.trim().length === 0){
+				throw "Must Enter A Valid State."
+			}
+			if (date_val.trim().length === 0){
+				throw "Must Enter A Valid Date."
+			}
+			if (new Date(date_val) > new Date()){
+				throw "Date Has Not Yet Occured. Please Enter A Valid Date.";	
+			}
+
 			var requestConfig = {
-			method: "POST",
-			url: "/private/userinfo",
-			contentType: "application/json",
-			data: JSON.stringify({
-				street: street_val,
-				state:state_val,
-				town: town_val,
-				date: date_val
-			})
+				method: "POST",
+				url: "/private/userinfo",
+				contentType: "application/json",
+				data: JSON.stringify({
+					street: street_val,
+					state:state_val,
+					town: town_val,
+					date: date_val
+				})
 			};
 
 			$.ajax(requestConfig).then(function(responseMessage) {
@@ -41,7 +57,16 @@
 				console.log(responseMessage);
 				
 			});
+
+
+
+		}catch(e){
+			error_div.append($('<p></p>').text(e));
+			error_div.addClass("error-userinfo");
 		}
+		
+		
+
 		
 	});
 	
--- routes/private.js
+++ routes/private.js
@@ -6,16 +6,18 @@ const geocoder = new Nominatim();
 
 router.get("/", async(req, res) => {
     if (req.session.user) {
-        //this needs to be updated with navbar and a private page
         res.render('layouts/main');
+        return;
     }
-
+    //do i have to add another case here, i get errors if i do
 });
 router.get("/userinfo", async(req, res) => {
     if (req.session.user) {
         let user_locations = await location.getUserLocations(req.session.user['UserID']);
-        console.log(user_locations);
-        res.render('private/userinfo', { username: req.session.user['UserID'], locations: user_locations });
+        res.render('private/userinfo', { username: req.session.user['UserID'], locations: user_locations});
+        return;
+    }else{
+        return res.redirect('/');
     }
 
 });
@@ -24,33 +26,44 @@ router.post("/userinfo", async(req, res) => {
     console.log(req.body);
     //must validate the form submission here
     
-    let date = new Date(req.body.date);
-    console.log(typeof(date));
-    console.log(date);
+    try{
+        if (req.body.street.trim().length === 0){
+            throw "Must Enter A Valid Street Address."
+        }
+        if (req.body.town.trim().length === 0){
+            throw "Must Enter A Valid Town."
+        }
+        if (req.body.state.trim().length === 0){
+            throw "Must Enter A Valid State."
+        }
+        if (req.body.date.length === 0){
+            throw "Must Enter A Valid Date."
+        }
+        if (new Date(req.body.date) > new Date()){
+            throw "Date Has Not Yet Occured. Please Enter A Valid Date.";   
+        }
+        let date = new Date(req.body.date);
 
+        geocoder.search({ street: req.body.street, city: req.body.town, state: req.body.state }).then((response) => {
+            //console.log("lat: " + response[0].lat);
+            //console.log("lon: " + response[0].lon);
+            //add to the database - create a location id and insert the location inside the location database
+            //add the location id into the users database
+
+            let newLocation = location.createLocation(req.session.user['UserID'], response[0].lon, response[0].lat, true, response[0].display_name,date);
+            res.render('partials/location_info', { geo: response[0].display_name, date: req.body.date });
+            return;
+        })
+
+
+    }catch(e){
+        //render a page with error
+        let user_locations = await location.getUserLocations(req.session.user['UserID']);
+        res.status(400).render('private/userinfo', { username: req.session.user['UserID'], locations: user_locations, error:e });
+        return;
+        //res.status(400).json(e);
+    }
    
-
-    /*if (date > new Date()){
-        //res.status(400).json({ error: 'You must provide a valid dateOfReviewthat is before today.' });
-        console.log("Date Has Not Yet Occured.");
-        throw "Date Has Not Yet Occured.";
-    }*/
-    geocoder.search({ street: req.body.street, city: req.body.town, state: req.body.state }).then((response) => {
-        console.log("lat: " + response[0].lat);
-        console.log("lon: " + response[0].lon);
-        //add to the database - create a location id and insert the location inside the location database
-        //add the location id into the users database
-
-        let newLocation = location.createLocation(req.session.user['UserID'], response[0].lon, response[0].lat, true, response[0].display_name,date);
-        res.render('partials/location_info', { geo: response[0].display_name, date: req.body.date });
-
-
-
-    }).catch((error) => {
-        console.log("search not able");
-        //throw an error
-        res.status(400).json({ error: error });
-    })
 
 });
 
