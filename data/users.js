63f76b87b29ed7f4a847da84368d63fd86819cc6
Avaneesh <akolluri@stevens.edu>
2021-05-04 11:56:07 -0400

    Validated users in data

--- data/users.js
+++ data/users.js
@@ -2,13 +2,24 @@ const mongoCollections = require('../config/mongoCollections');
 const users = mongoCollections.users;
 const bcrypt = require('bcrypt');
 const { get } = require('../routes/private');
+const valid = require('./validate');
 const saltRounds = 16;
 
 let methods = {
 
 	async Create(email,username, password) {
 
-		if (!email) throw 'You must provide an email.';
+
+		if(valid.userID(username) == false){
+			throw 'Username should be an alphanumeric string between 6 to 16 characters';
+		}
+		if(valid.password(password) == false){
+			throw 'Password should have 1 lowercase letter, 1 uppercase letter, 1 number, and be at least 8 characters long';
+		}
+		if(valid.email(email) == false){
+			throw 'Email should be of the proper email format';
+		}
+		/*if (!email) throw 'You must provide an email.';
 		if (!username) throw 'You must provide an username.';
 		if (!password) throw 'You must provide an password.';
 
@@ -21,7 +32,7 @@ let methods = {
 
 		if (typeof(password) != 'string' || password.trim().length === 0 ){
 			throw 'You must provide a password that is more than just empty spaces.';
-		}
+		}*/
 		
 		//must validate all fields before (how to validate email)
 
