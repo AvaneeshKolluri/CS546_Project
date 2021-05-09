c2bfd1bdc7e25bc6e52a958cf181101f29f26246
Vincenzo Susi <vsusi@stevens.edu>
2021-05-09 12:35:44 -0400

    edited routes index.js for quiz

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const publicRoutes = require('./public.js');
 const apiRoutes = require('./api');
 const mainRoutes = require('./main');
 const createRoutes = require('./create');
+const quizRoutes = require('./quiz');
 
 
 const constructorMethod = (app) => {
@@ -11,6 +12,7 @@ const constructorMethod = (app) => {
   app.use('/public', publicRoutes);
   app.use('/api', apiRoutes);
   app.use('/create', createRoutes);
+  app.use('/quiz', quizRoutes);
 
   app.use('*', (req, res) => {
     res.sendStatus(404); // Add some page here
